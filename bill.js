248361b37e6d83b9048687d78b4454314c115a96
windiarties <windiarties@gmail.com>
2020-07-01 10:26:36 +0700

    edit attr

--- bill.js
+++ bill.js
@@ -3,14 +3,12 @@ const axios = require('axios')
 
 axios.get('https://billboardid.com/top100/').then((response) => {
     const $ = cheerio.load(response.data)
-
-
     const data = $('.foo-table tr')
     console.log("data billboard : ", data.length)
 
     for (let i = 0; i < data.length; i++) {
         const pos = (i+1)
-        const judul = $(data[i+1]).find('.ninja_column_2, h6').text().replace('▬', '')
+        const judul = $(data[i+1]).find('h6').text().replace('▬', '')
 		//const penyanyi = $(data[i]).find('').text()
        // const judul = $(data[i]).find('.song-name.typography-label')[0].text()
         //const penyanyi = $(data[i]).find('.by-line.typography-caption')[0].text()
