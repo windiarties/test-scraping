3a77129d0ec7262b38e91b787daf9e57f7b4019f
windiarties <windiarties@gmail.com>
2020-07-01 11:50:34 +0700

    add const judul, penyanyi and change text

--- bill.js
+++ bill.js
@@ -8,19 +8,10 @@ axios.get('https://billboardid.com/top100/').then((response) => {
 
     for (let i = 0; i < data.length; i++) {
         const pos = (i + 1)
-        const judul = $(data[i + 1]).find('h6').text().replace('▬', '')
-        const judulFix = judul.slice(judul.indexOf('-') + 1, judul.length).trim()
-        
-        //const penyanyi = $(data[i]).find('').text()
-        // const judul = $(data[i]).find('.song-name.typography-label')[0].text()
-        //const penyanyi = $(data[i]).find('.by-line.typography-caption')[0].text()
+        const list = $(data[i + 1]).find('strong').text().replace('⇧', '').replace('⇩', '').replace('≡', '').replace('⇳', '').replace('☆', '').replace(new RegExp('[0-9]', 'g'), '')
+        const judul = list.slice(0, list.indexOf('▬')).trim()
+        const penyanyi = list.slice(list.indexOf('▬') + 1, list.length).trim()
 
-        // if (judul, penyanyi, pos) {
-        //     //const urlText = $(pos).text()
-        //     const urlText1 = $(judul).text()
-        //     const urlText2 = $(penyanyi).text()
-
-        //     console.log(pos, urlText1, urlText2)
-        console.log(pos, judulFix)   // }
+        console.log(pos, judul, penyanyi)   
     }
 })
