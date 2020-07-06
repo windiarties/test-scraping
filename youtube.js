3d4aee92db702fd8c60c6a0bd2050342695d6604
windiarties <windiarties@gmail.com>
2020-07-06 13:01:25 +0700

    add $

--- youtube.js
+++ youtube.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 axios.get('https://www.youtube.com/user/musicastudios/videos').then((response) => {
 
     const $ = cheerio.load(response.data)
-    var data = $('#channels-browse-content-grid li .yt-lockup-video');
+    var data = $('ytd-grid-video-renderer');
     console.log("data youtube : ", data.length)
 
     for (let i = 0; i < data.length; i++) {
@@ -12,6 +12,7 @@ axios.get('https://www.youtube.com/user/musicastudios/videos').then((response) =
         // const list = $(data[i + 1]).find('strong').text().replace('⇧', '').replace('⇩', '').replace('≡', '').replace('⇳', '').replace('☆', '').replace(new RegExp('[0-9]', 'g'), '')
         const judul = $('#eow-title').text();
         const judulFIX = judul.slice(judul.indexOf('-') + 1, judul.length).trim();
+        const version = judulFIX.indexOf('(') !== -1 ? judulFIX.slice(judulFIX.indexOf('('), judulFIX.length) : judulFIX.indexOf('|') !== -1 ? judulFIX.slice(judulFIX.indexOf('|'), judulFIX.length) : judulFIX.indexOf('-') !== -1 ? judulFIX.slice(judulFIX.indexOf('-'), judulFIX.length) : judulFIX.indexOf('[') !== -1 ? judulFIX.slice(judulFIX.indexOf('['), judulFIX.length) : '-'
         const penyanyi = judul.slice(0, judul.indexOf('-')).trim();
         const penyanyiFIX = penyanyi.replace(version, '').trim();
 
