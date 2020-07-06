b08bcb0d7a2c380fe2fbf02c7f6f7cc35dc7f05b
windiarties <windiarties@gmail.com>
2020-07-06 12:02:26 +0700

    edit cheerio

--- apple2.js
+++ apple2.js
@@ -12,6 +12,8 @@ axios.get('https://music.apple.com/pg/playlist/top-100-indonesia/pl.2b7e089dc9ef
         const pos = (i + 1)
         const judul = $(data[i]).find('div.col-song div.col-song__wrapper div.two-lines div.song-name-wrapper div.typography-label').text()
         const penyanyi = $(data[i]).find('div.col-song div.col-song__wrapper div.two-lines div.song-name-wrapper div.typography-caption a.dt-link-to').text()
+        
+        
         // const judul = $(data[i]).find('.song-name.typography-label')[0].text()
         //const penyanyi = $(data[i]).find('.by-line.typography-caption')[0].text()
 
