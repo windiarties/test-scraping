6fdb2f2daa8ad8642294e4e40b24b2dfe842cf1e
windiarties <windiarties@gmail.com>
2020-07-22 01:51:04 +0700

    edit attr

--- youtube.js
+++ youtube.js
@@ -1,31 +1,24 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+axios.get('https://www.youtube.com/c/MusicaStudios/videos').then((response) => {
+    const $ = cheerio.load(response.data)
+    const data = $('.ytd-grid-video-renderer, a')
+    console.log("data : ", data.length)
+    console.log(data.text())
+    
 
-var url = "https://www.youtube.com/c/MusicaStudios/videos";
+    // for (let i = 0; i < data.length; i++) {
+    //     const pos = (i + 1)
+    //     const judul = $('#eow-title').text();
+    //     const judulFIX = judul.slice(judul.indexOf('-') + 1, judul.length).trim();
+    //     const version = judulFIX.indexOf('(') !== -1 ? judulFIX.slice(judulFIX.indexOf('('), judulFIX.length) : judulFIX.indexOf('|') !== -1 ? judulFIX.slice(judulFIX.indexOf('|'), judulFIX.length) : judulFIX.indexOf('-') !== -1 ? judulFIX.slice(judulFIX.indexOf('-'), judulFIX.length) : judulFIX.indexOf('[') !== -1 ? judulFIX.slice(judulFIX.indexOf('['), judulFIX.length) : '-'
+    //     const penyanyi = judul.slice(0, judul.indexOf('-')).trim();
+    //     const penyanyiFIX = penyanyi.replace(version, '').trim();
 
-axios.get(url).then((err, response) => {
+    //     console.log(pos, judulFIX, penyanyiFIX)
+    // }
 
-    console.log("ini url: ", url)
-
-    const $ = cheerio.load(response.data);
-    console.log("respon : ", response.data)
-
-    var data = $('a').filter('.video-title').attr('id');
-
-    console.log("pjg data :", data.length)
-
-    for (let i = 0; i < data.length; i++) {
-        const pos = (i + 1)
-        const judul = $('#eow-title').text();
-        const judulFIX = judul.slice(judul.indexOf('-') + 1, judul.length).trim();
-        const version = judulFIX.indexOf('(') !== -1 ? judulFIX.slice(judulFIX.indexOf('('), judulFIX.length) : judulFIX.indexOf('|') !== -1 ? judulFIX.slice(judulFIX.indexOf('|'), judulFIX.length) : judulFIX.indexOf('-') !== -1 ? judulFIX.slice(judulFIX.indexOf('-'), judulFIX.length) : judulFIX.indexOf('[') !== -1 ? judulFIX.slice(judulFIX.indexOf('['), judulFIX.length) : '-'
-        const penyanyi = judul.slice(0, judul.indexOf('-')).trim();
-        const penyanyiFIX = penyanyi.replace(version, '').trim();
-
-        console.log(pos, judulFIX, penyanyiFIX)
-    }
-
-    console.log(err)
+    // console.log(err)
 })
 
